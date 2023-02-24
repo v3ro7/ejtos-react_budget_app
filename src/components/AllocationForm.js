b9930f745df050d8bb6848cbd66cefc07a8e7869
v3ro7 <veronicapiriz33@gmail.com>
2023-02-24 16:19:34 -0500

    task1-part1

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -55,15 +55,16 @@ const AllocationForm = (props) => {
                         <option defaultValue value='Add' name='Add'>Add</option>
                         <option value='Reduce' name='Reduce'>Reduce</option>
                     </select>
-
+                        £
                         <input
                             required='required'
                             type='number'
                             id='cost'
                             value={cost}
-                            style={{ marginLeft: '2rem', size: 10 }}
+                            style={{marginLeft: '2rem', size: 10 }}
                             onChange={(event) => setCost(event.target.value)}
                         />
+                        
 
                         <button className='btn btn-primary' onClick={submitEvent} style={{ marginLeft: '2rem' }}>
                             Save
